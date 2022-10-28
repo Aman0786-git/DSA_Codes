c34a0f7b6ac0b55199222d36173788130a4a0084
Aman <aman999.au@gmail.com>
2022-10-28 23:27:24 +0530

    update

---
+++ 11. Linked List/40__Circular__LL.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+
+} * head;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    head = (struct Node *)malloc(sizeof(struct Node));
+    head->data = A[0];
+    head->next = head;
+    last = head;
+
+    for (i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = last->next;
+        last->next = t;
+        last = t;
+    }
+}
+int main()
+{
+
+    return 0;
+}
