89d0eafd17281233fcd0a058f72b4e1b85409c72
Aman <aman999.au@gmail.com>
2022-10-30 20:51:57 +0530

    update

--- 11. Linked List/40__Circular__LL.c
+++ 11. Linked List/40__Circular__LL.c
@@ -25,8 +25,80 @@ void create(int A[], int n)
         last = t;
     }
 }
+
+void Display(struct Node *h)
+{
+    do
+    {
+        printf("%d ", h->data);
+        h = h->next;
+
+    } while (h != head);
+    printf("\n");
+}
+void Rdisplay(struct Node *h)
+{
+    static int flag = 0;
+    if (h != head || flag == 0)
+    {
+        flag = 1;
+        printf("%d ", h->data);
+        Rdisplay(h->next);
+    }
+    flag = 0;
+}
+
+int Length(struct Node *p)
+{
+    int len = 0;
+    do
+    {
+        len++;
+        p = p->next;
+    } while (p != head);
+    return len;
+}
+void Insert(struct Node *p, int index, int x)
+{
+    struct Node *t;
+    int i;
+    if (index < 0 || index > Length(head))
+        return;
+    if (index == 0)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = x;
+        if (head == NULL)
+        {
+            head = t;
+            head->next = head;
+        }
+        else
+        {
+            while (p->next != head)
+                p = p->next;
+            p->next = t;
+            t->next = head;
+            head = t;
+        }
+    }
+    else
+    {
+        for (i = 0; i < index - 1; i++)
+            p = p->next;
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = x;
+        t->next = p->next;
+        p->next = t;
+    }
+}
 int main()
 {
-
+    int A[] = {1, 2, 3, 4, 5};
+    create(A, 5);
+    // Insert(head, 3, 10);
+    Insert(head, 0, 10);
+    Display(head);
+    // Rdisplay(head);
     return 0;
 }
