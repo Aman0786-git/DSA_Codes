3b411ca741c3fbb8eb1bee7affed384032be8553
Aman <aman999.au@gmail.com>
2022-11-10 20:23:08 +0530

    update

--- 11. Linked List/53__Circular_Doubly_LL.c
+++ 11. Linked List/53__Circular_Doubly_LL.c
@@ -15,7 +15,8 @@ void create(int A[], int n)
 
     first = (struct Node *)malloc(sizeof(struct Node));
     first->data = A[0];
-    first->prev = first->next = NULL;
+    first->prev = first;
+    first->next = first;
     last = first;
     for (i = 1; i < n; i++)
     {
@@ -30,11 +31,12 @@ void create(int A[], int n)
 
 void Display(struct Node *p)
 {
-    while (p)
+    do
     {
         printf("%d ", p->data);
         p = p->next;
-    }
+
+    } while (p != first);
     printf("\n");
 }
 
@@ -78,55 +80,10 @@ void Insert(struct Node *p, int index, int x)
     }
 }
 
-int Delete(struct Node *p, int index)
-{
-    struct Node *q;
-    int x = -1, i;
-
-    if (index < 1 || index > Length(p))
-        return -1;
-    if (index == 1)
-    {
-        first = first->next;
-        if (first)
-            first->prev = NULL;
-        x = p->data;
-        free(p);
-    }
-    else
-    {
-        for (i = 0; i < index - 1; i++)
-            p = p->next;
-        p->prev->next = p->next;
-        if (p->next)
-            p->next->prev = p->prev;
-        x = p->data;
-        free(p);
-    }
-    return x;
-}
-
-void Reverse(struct Node *p)
-{
-    struct Node *temp;
-    while (p != NULL)
-    {
-        temp = p->next;
-        p->next = p->prev;
-        p->prev = temp;
-        p = p->prev;
-        if (p != NULL && p->next == NULL)
-            first = p;
-    }
-}
 int main()
 {
     int A[] = {10, 20, 30, 40, 50};
     create(A, 5);
-    printf("Length is : %d\n", Length(first));
-    Insert(first, 0, 25);
-    Delete(first, 1);
-    Reverse(first);
     Display(first);
     return 0;
 }
