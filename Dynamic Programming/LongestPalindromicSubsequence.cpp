8f27b663db77fdb0225a7e08698cb6fff21dc78c
Aman <aman999.au@gmail.com>
2024-08-23 09:25:08 +0530

    Longest Palindromic Subsequence

---
+++ Dynamic Programming/LongestPalindromicSubsequence.cpp
@@ -0,0 +1,26 @@
+//Longest Palindromic Subsequence
+// LPS === LCS(a,reverse(a)); a-> string
+
+int lcs(int n, int m , string str1, string str2) {
+	vector<vector<int>>t(n + 1, vector<int>(m + 1));
+
+	for (int i = 0; i <= n; i++)t[i][0] = 0;
+	for (int j = 0; j <= m; j++)t[0][j] = 0;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (str1[i - 1] == str2[j - 1])
+				t[i][j] = 1 + t[i - 1][j - 1];
+			else
+				t[i][j] = max(t[i][j - 1], t[i - 1][j]);
+		}
+	}
+	return t[n][m];
+}
+
+int longestPalindromeSubseq(string str1) {
+	int n = str1.length();
+	string str2 = str1;
+	reverse(str2.begin(), str2.end());
+	return lcs(n, n, str1, str2);
+}
